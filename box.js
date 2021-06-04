53202bc6bb5fe7e4196b22f49dee20783a8d3a27
Sohamambre07 <ambresoham06@gmail.com>
2021-06-04 22:55:52 +0530

    Project 32

---
+++ box.js
@@ -0,0 +1,24 @@
+class Box{
+constructor(x,y,w,h){
+
+    var options={
+
+       friction:0,
+       restitution:0.5
+    }
+  this.body=Bodies.rectangle(x,y,w,h,options)
+  World.add(world,this.body)
+this.width=w 
+this.height=h
+}
+
+display(){
+
+push()
+translate(this.body.position.x,this.body.position.y)
+rotate(this.body.angle)
+rectMode(CENTER)
+rect(0,0,this.width,this.height)
+pop()
+}
+}
